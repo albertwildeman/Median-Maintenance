3ee5491394b3299689e89a98bd391905067b0e12
Albert Wildeman <albert.wildeman@gmail.com>
2016-09-06 21:33:38 -0500

    Median maintenance using 2 heaps. Code complete but Heap.extractmin() is broken.

--- MedianMaintenanceLib.py
+++ MedianMaintenanceLib.py
@@ -31,30 +31,57 @@ class Heap(object):
         iParent = int(iVal/2)
 
         # bubble up
-        while keys[iVal] < keys[iParent] and iVal>0:
-            keys[iVal], keys[iParent] = keys[iParent], keys[iVal]
+        while self.keys[iVal] < self.keys[iParent] and iVal>0:
+            self.keys[iVal], self.keys[iParent] = self.keys[iParent], self.keys[iVal]
             iVal = iParent
-            iParent = int(iVal/2)
-
+            iParent = int((iVal-1)/2)
 
     def extractmax(self):
-        self.extractmin(self)
+        self.extractmin()
 
     def extractmin(self):
 
         # swap last element into first position
-        keys[0] = keys[-1]
+        self.keys[0] = self.keys[-1]
         # delete last element
-        del keys[-1]
+        del self.keys[-1]
 
         # bubble down
+        iVal = 0
+        iChild1 = 1
+        iChild2 = 2
+        done = False
+
+        while (self.keys[iVal] > self.keys[iChild1] or  self.keys[iVal] > self.keys[iChild2]) and not done:
+
+            # Bubble down. Pick the child to swap with to be the smaller one
+            if self.keys[iChild1] < self.keys[iChild2]:
+                self.keys[iChild1], self.keys[iVal] = self.keys[iVal], self.keys[iChild1]
+                iVal = iChild1
+            else:
+                self.keys[iChild2], self.keys[iVal] = self.keys[iVal], self.keys[iChild2]
+                iVal = iChild2
+
+            # Update children
+            iChild1 = iVal * 2 + 1
+            iChild2 = iVal * 2 + 2
+
+            # If new position has 1 or 0 kids, do final swap if necessary and then exit the while loop.
+            if iChild2 >= len(self.keys):
+                if iChild1 < len(self.keys): # val only has one child. Bubble down if necessary
+                    if self.keys[iVal] > self.keys[iChild1]:
+                        # swap
+                        self.keys[iChild1], self.keys[iVal] = self.keys[iVal], self.keys[iChild1]
+                done = True
 
 
 
 def median_maintenance(a):
+    # Implementation of dual heap technique for median maintenance
+
 
     # Initialize list to hold the return list with maintained medians
-    medians = len(a) * []
+    medians = len(a) * [int(0)]
 
     # Initialize 2 empty heaps,
     # one to hold the smaller half of the processed part of the array,
@@ -63,8 +90,8 @@ def median_maintenance(a):
     minheap = Heap("min")
 
     # Add one of the first two values to a to each of the heaps
-    maxhead.insert(min(a[:1]))
-    minhead.insert(max(a[:1]))
+    maxheap.insert(min(a[:1]))
+    minheap.insert(max(a[:1]))
     # Set the corresponding medians
     medians[0] = a[0]
     medians[1] = a[0]
